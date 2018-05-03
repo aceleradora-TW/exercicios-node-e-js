97120c67320914f55aee53c75eed66e11a8ad219
othman853 <anuar.yasser@gmail.com>
2018-05-03 18:24:43 -0300

    Resolve acumulador

--- test/acumulador.test.js
+++ test/acumulador.test.js
@@ -5,26 +5,27 @@
 const acumulador = require('../src/acumulador.js')
 
 describe('Acumulador', () => {
-    it('deve retornar mensagem de erro se o array é vazio', () => {
-        expect(acumulador([])).toEqual("Array não pode ser vazio")
-    })
+  it('deve retornar mensagem de erro se o array é vazio', () => {
+      expect(acumulador([])).toEqual('Array não pode ser vazio')
+  })
 
-    it('deve acumular todos os numeros num array', () => {
-        expect(acumulador([1,2,3,4,5])).toEqual(15)
-    })
+  it('deve acumular todos os numeros num array', () => {
+      expect(acumulador([1,2,3,4,5])).toEqual(15)
+  })
 
-    describe('logger', () => {
-        /** 
-        * É obrigatório usar o logger.js nesta parte do exercício. 
-        */
-       
-        global.console = {
-            log: jest.fn()
-        }
+})
 
-        it('tambem deve colocar a resposta no log após acumular', () => {
-            acumulador([1])
-            expect(global.console.log).toBeCalledWith(1)
-        })
-    })
+describe('logger', () => {
+  /**
+  * É obrigatório usar o logger.js nesta parte do exercício.
+  */
+
+  global.console = {
+      log: jest.fn()
+  }
+
+  it('tambem deve colocar a resposta no log após acumular', () => {
+      acumulador([1, 2])
+      expect(global.console.log).toBeCalledWith(3)
+  })
 })
