31bb57f77162b797023250cc8fbd236c14338be8
laurascclaro <laurascclaro@hotmail.com>
2018-05-03 14:44:11 -0300

    implementa funcoes soma e subtracao

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,10 +1,11 @@
 
 function soma(umValor, outroValor) {
-  // Implemente
+  return umValor+outroValor
+
 }
 
 function subtrai(umValor, outroValor) {
-  // Implemente
+  return umValor-outroValor
 }
 
 module.exports = {soma, subtrai}
