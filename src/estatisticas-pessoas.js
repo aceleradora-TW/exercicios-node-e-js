cee079d924d5f570099635f3c6f40ae3a2de74b2
laurascclaro <laurascclaro@hotmail.com>
2018-05-03 15:59:03 -0300

    implementa possuiPetChamado

--- src/estatisticas-pessoas.js
+++ src/estatisticas-pessoas.js
@@ -4,8 +4,29 @@ function maiorSalario(){
   let maior=0
   for (let i=0; i < pessoas.length; i++){
     if (pessoas[i].salario > maior)
-    maior = pessoas[i].salario
+      maior = pessoas[i].salario
   }
   return maior
 }
-module.exports ={maiorSalario}
+
+function possuiPetChamado(nomePet){
+     return pessoas.filter((pessoa)=> {
+      return pessoa.pets.some((pet)=>{
+        return pet.nome===nomePet
+       })
+     })[0]
+}
+console.log (possuiPetChamado('Vieirinha'))
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports ={maiorSalario, possuiPetChamado}
