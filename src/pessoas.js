2a4e8bd68a9d802c3f6850daa756567fd72b98b0
othman853 <anuar.yasser@gmail.com>
2018-05-03 14:45:21 -0300

    Pessoas e estatísticas

---
+++ src/pessoas.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    nome: 'Silva',
+    idade: 30,
+    salario: 1500,
+    pets: []
+  },
+  {
+    nome: 'Lopes',
+    idade: 68,
+    salario: 2000,
+    pets: [{
+      nome: 'Qwerty',
+      idade: 10,
+      tipo: 'Peixe'
+    }]
+  },
+  {
+    nome: 'Cruz',
+    idade: 12,
+    salario: 0,
+    pets: []
+  },
+  {
+    nome: 'Vieira',
+    idade: 31,
+    salario: 6200,
+    pets: [{
+      nome: 'Vieirinha',
+      idade: 4,
+      tipo: 'Cachorro'
+    }]
+  },
+]
