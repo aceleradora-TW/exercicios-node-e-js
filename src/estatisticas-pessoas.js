98c0a1e06f2d9211400f20864e18e1fe30c25d6e
marcos012 <marcospaulo.ad@gmail.com>
2018-05-04 14:58:56 -0300

    Testes passando com sucesso

--- src/estatisticas-pessoas.js
+++ src/estatisticas-pessoas.js
@@ -1,21 +1,4 @@
-// for (var i = 0; i < estatisticas; i++) {
-//   val = estatisticas[i]
-//   console.log(val.path)
-// }
-// for (var i = 0; i < listaPessoas.length; i++) {
-//   if (listaPessoas[i].salario > campoSalario) {
-//     for (var i = 0; i < pets.length; i++) {
-//       campoNome = listaPessoas[i].nome
-//       campoIdade = listaPessoas[i].idade
-//       campoSalario = listaPessoas[i].salario
-//     }
-//   }
-// }
-// console.log(campoNome,campoIdade,campoSalario);
-
 let listaPessoas = require('./pessoas')
-let campoNome = ""
-let campoIdade = ""
 let campoSalario = 0
 
 function maiorSalario(){
@@ -31,6 +14,13 @@ const possuiPetChamado = (nomeDoPet) => {
   return listaPessoas.find(pessoa => pessoa.pets.some(pet => pet.nome === nomeDoPet))
 }
 
+const possuiNenhumPet = () => {
+  const listaFiltrada = listaPessoas.filter(pessoa => pessoa.pets.length == 0)
+  return listaFiltrada.map(pessoa => pessoa.nome)
+
+}
+
+console.log(possuiNenhumPet())
 
 
-module.exports = {maiorSalario,possuiPetChamado}
+module.exports = {maiorSalario,possuiPetChamado,possuiNenhumPet}
