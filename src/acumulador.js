a90c2a2acd2db0c86ef5ffc6b70e208446770ecf
laurascclaro <laurascclaro@hotmail.com>
2018-05-04 11:41:30 -0300

    implementando logger

--- src/acumulador.js
+++ src/acumulador.js
@@ -1,15 +1,16 @@
+const logger = require('./logger.js')
 function verificaVazio(array){
-  if (array.length == 0){
-    return "Array não pode ser vazio"
-  }
+ if (array.length == 0){
+   return "Array não pode ser vazio"
+ }
 }
 
-
 function acumulaNum(array){
-  let soma = 0
-  array.forEach(function (valor){
-    soma += valor
-  })
-  return soma
+ let soma = 0
+ array.forEach(function (valor){
+   soma += valor
+ })
+ logger(soma)
+ return soma
 }
 module.exports = {verificaVazio, acumulaNum}
